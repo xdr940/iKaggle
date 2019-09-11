ad5f7fb9f1f2bebc98376b09110f5c7a8851b093
xdr940 <xdr94@126.com>
2019-09-11 09:21:45 +0800

    add scikit-learn datasets checking

---
+++ House/preprocess.py
@@ -0,0 +1,25 @@
+from sklearn.datasets import load_digits
+from sklearn.datasets import load_iris
+from sklearn.datasets import load_diabetes
+from sklearn.datasets import load_breast_cancer
+import numpy as np
+import matplotlib.pyplot as plt 
+#X,y = load_digits(return_X_y=True)
+#iris_dat= load_iris()
+#X3,y3 = load_diabetes(return_X_y=True)
+#bc_dat = load_breast_cancer()
+#db_dat = load_diabetes()
+
+
+from sklearn.datasets import load_wine
+dat_wine = load_wine()
+
+
+#X5,y5 = load_iris(return_X_y=True)
+
+#test = np.concatenate([bc_dat.data,bc_dat.target])
+
+
+
+
+print('ok')
